8ddb11dd6b2d8680318ba7ed9ba41c97335ffd22
Salomon Sickert <sickert@in.tum.de>
2015-11-27 13:28:28 +0100

    Restructure project and automate testing

---
+++ src/test/java/TestAutomata.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import rabinizer.automata.MasterFolded;
+import rabinizer.bdd.BDDForVariables;
+import rabinizer.formulas.Formula;
+import rabinizer.formulas.FormulaFactory;
+
+public class TestAutomata {
+
+	@Test
+	public void testMasterFoldedNew() {
+		Formula f1 = FormulaFactory.mkLit("p1", BDDForVariables.bijectionIdAtom.id("p1"), false);
+		Formula f2 = FormulaFactory.mkLit("p1", BDDForVariables.bijectionIdAtom.id("p1"), false);
+		Formula f3 = FormulaFactory.mkOr(f1, f2);
+		Formula f4 = FormulaFactory.mkG(f3);
+
+		MasterFolded m = new MasterFolded(f4);
+		assertEquals(f4, m.formula);
+	}
+}
