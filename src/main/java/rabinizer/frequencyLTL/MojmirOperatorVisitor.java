15b7827e04b362a65a3877bed1e10abee8ac9ba3
ga25suc <ga25suc@mytum.de>
2016-08-05 12:51:26 +0200

    removed unneccessary method

--- src/main/java/rabinizer/frequencyLTL/MojmirOperatorVisitor.java
+++ src/main/java/rabinizer/frequencyLTL/MojmirOperatorVisitor.java
@@ -2,9 +2,6 @@ package rabinizer.frequencyLTL;
 
 import ltl.visitors.DefaultConverter;
 import ltl.FOperator;
-import ltl.Formula;
-import ltl.FrequencyG;
-import ltl.GOperator;
 
 /**
  * This Visitor visits a formula and it replaces 'ordinary' F-operators by
@@ -16,14 +13,5 @@ public class MojmirOperatorVisitor extends DefaultConverter {
     @Override
     public FOperatorForMojmir visit(FOperator fOperator) {
         return new FOperatorForMojmir(fOperator.operand.accept(this));
-    }
-
-    @Override
-    public Formula visit(GOperator gOperator) {
-        if (gOperator instanceof FrequencyG) {
-            FrequencyG freq = (FrequencyG) gOperator;
-            return new FrequencyG(gOperator.operand.accept(this), freq.bound, freq.cmp, freq.limes);
-        }
-        return super.visit(gOperator);
     }
 }
